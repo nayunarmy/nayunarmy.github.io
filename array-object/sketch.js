62c8307dfe3953c275c46b05ba2788c75883ec9e
nayunarmy <navyasauhta@gmail.com>
2024-10-14 21:45:49 -0600

    created simple input tabs
    learned how the inputName thing works over the weekend and tried it out .

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -1,41 +1,45 @@
-// Tic-Tac-Toe
+// simple shopping list
 // Navya Sauhta
 // 21 oct 2024
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - use of buttons function in p5js
 
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-// }
 
-// function draw() {
-//   background(220);
-// }
 
-// Setup function to initialize the game
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+  // Create input fields for item name, quantity, and price
+  inputName = createInput();
+  inputName.position(20, 40);
+  inputName.attribute('placeholder', 'Item Name');
+  
+  inputQuantity = createInput();
+  inputQuantity.position(150, 40);
+  inputQuantity.attribute('placeholder', 'Quantity');
+  
+  inputPrice = createInput();
+  inputPrice.position(280, 40);
+  inputPrice.attribute('placeholder', 'Price');
+  
 
-  theBoard = {'7': ' ' , '8': ' ' , '9': ' ' ,
-    '4': ' ' , '5': ' ' , '6': ' ' ,
-    '1': ' ' , '2': ' ' , '3': ' ' }
-  }
-    
-
-
-// Draw function to output the board in the console (for now)
-function draw() {
-
-
-
-deff printBoard(board):
-print(board['7'] + '|' + board['8'] + '|' + board['9'])
-print('-+-+-')
-print(board['4'] + '|' + board['5'] + '|' + board['6'])
-print('-+-+-')
-print(board['1'] + '|' + board['2'] + '|' + board['3'])
-  draw(); // Draw the initial empty board
 }
+
+function draw() {
+  background(220);
+  displayShoppingList();
+}
+
+// Function to display the shopping list
+function displayShoppingList() {
+  textSize(16);
+  fill(0);
+  text("Shopping List:", 20, 100);
+}
+
+
+
   
 
   
