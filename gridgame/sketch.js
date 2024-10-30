95820a26c78c4add24dd7c4565bae749f33f7f2f
nayunarmy <navyasauhta@gmail.com>
2024-10-30 14:16:09 -0600

    made opening page and buttons
    decided on making slang wordle and made the basic opening page.

--- gridgame/sketch.js
+++ gridgame/sketch.js
@@ -7,13 +7,89 @@
 
 let boardSize = 400;
 let cellSize = boardSize / 7;
-let players = 
-
+let listPage = 'start';
+let startButton;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  //making buttons
+  startButton = createButton('start');
+  startButton.position(width / 2 - 80, height / 2);
+  startButton.mouseClicked(optionPage);
+
+  optionButton1 = createButton('gen-z slang');
+  optionButton1.position(width / 2 - 80, height / 2);
+  optionButton1.mousePressed(genzWordle);
+
+  optionButton2 = createButton('gen-a slang');
+  optionButton2.position(width / 2 - 80, height / 1.8 );
+  optionButton2.mousePressed(genaWordle);
 }
 
 function draw() {
   background(220);
+  whichPage();
 }
+
+function whichPage(){
+  if (listPage === 'start'){
+    //opening page
+    textSize(32);
+    fill(0);
+    textAlign(CENTER, CENTER);
+    text('welcome to wordle ', width / 2, height / 2 - 50);
+  }
+  else if ( listPage === 'genzWorld'){
+    genzWordle();
+    optionButton1.hide();
+    optionButton2.hide();
+    startButton.hide();
+  }
+  else if ( listPage === 'genaWordle'){
+    genaWordle();
+    optionButton1.hide();
+    optionButton2.hide();
+    startButton.hide();
+  }
+  else if ( listPage === 'optionPage'){
+    optionPage();
+    optionButton1.hide();
+    optionButton2.hide();
+    startButton.hide();
+  }
+}
+
+function optionPage(){
+  listPage = 'optionPage';
+  //opening page
+  textSize(32);
+  fill(0);
+  textAlign(CENTER, CENTER);
+  text('welcome to wordle ', width / 2, height / 2 - 50);
+
+}
+
+function genaWordle(){
+  listPage = 'genaWordle';
+  fill(255);
+  for (let i = 0; i < 7; i++) {
+    for (let j = 0; j < 7; j++) {
+      rect(i * cellSize, j * cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function genzWordle(){
+  listPage = 'genzWorld';
+  fill(255);
+  for (let i = 0; i < 7; i++) {
+    for (let j = 0; j < 7; j++) {
+      rect(i * cellSize, j * cellSize, cellSize, cellSize);
+    }
+  }
+
+}
+
+
+
